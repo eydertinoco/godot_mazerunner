4c7bd4cc26d88b59246d3864172c47ac4b2dba1f
eyder <eydertinoco@outlook.com>
2024-06-21 08:18:08 -0300

    TA FUNCIONANDO ESSE CARALHOgit statusgit statusgit statusgit status

--- scripts/maze.gd
+++ scripts/maze.gd
@@ -9,11 +9,15 @@ extends Node2D
 @onready var saida := $Saida as Area2D
 
 var map_size = Vector2(16*Globals.nivelJogo, 8*Globals.nivelJogo)
-var maze = [] # Matriz para representar o labirinto
-var visited = [] # Matriz para acompanhar as células visitadas
-
-var player_start = Vector2(1, 1) # Posição inicial do jogador
-var exit_point = Vector2((16*Globals.nivelJogo)-1, (8*Globals.nivelJogo)-1) # Posição de saída do labirinto
+# Matriz para representar o labirinto
+var maze = [] 
+# Matriz para acompanhar as células visitadas
+var visited = [] 
+# Posição inicial do jogador
+var player_start = Vector2(1, 1) 
+# Posição de saída do labirinto
+#var exit_point = Vector2((16*Globals.nivelJogo)-1, (8*Globals.nivelJogo)-1)
+var exit_point = Vector2(randi() % int(map_size.x), randi() % int(map_size.y))
 
 func _ready():
 	Globals.player = player
@@ -35,20 +39,20 @@ func initialize_maze():
 		var row = []
 		var visited_row = []
 		for y in range(map_size.y):
-			row.append(0) # Todas as células começam como paredes
+			row.append(1) # Todas as células começam como paredes
 			visited_row.append(false) # Todas as células são marcadas como não visitadas
 		maze.append(row)
 		visited.append(visited_row)
-	print('Mapa do labrinto ao ser inicializado:')
-	print(maze)
+	#print('Mapa do labrinto ao ser inicializado:')
+	#print(maze)
 
 # Gerar labirinto
 func generate_maze():
 	# Pilha para acompanhar as células visitadas
 	var stack = []
 	# Célula inicial aleatória
-	# var current_cell = Vector2(randi() % int(map_size.x), randi() % int(map_size.y))
-	var current_cell = player_start
+	var current_cell = Vector2(randi() % int(map_size.x), randi() % int(map_size.y))
+	# var current_cell = player_start
 	visited[current_cell.x][current_cell.y] = true
 	while has_unvisited_cells():
 		# Observa os campos ao redor dele
@@ -62,8 +66,8 @@ func generate_maze():
 		elif stack.size() > 0:
 			# Não há mais caminho, ele retorna para campo anterior.
 			current_cell = stack.pop_back()
-	print('Mapa Gerado')
-	print(maze)
+	#print('Mapa Gerado')
+	#print(maze)
 
 # Essa função vai procurar por coordenadas que não foram visitadas
 func has_unvisited_cells():
@@ -87,37 +91,42 @@ func get_unvisited_neighbors(cell):
 # Conectar o começo ao fim
 func connect_start_to_exit():
 	var start_cell = player_start
-	var exit_cell = Vector2(exit_point.x - 1, exit_point.y - 1) # Ajustando para as coordenadas do labirinto gerado
+	# Ajustando para as coordenadas do labirinto gerado
+	#var exit_cell = Vector2(exit_point.x - 1, exit_point.y - 1) 
+	# Célula inicial aleatória
+	var exit_cell = exit_point
 	# Use BFS para encontrar um caminho do início até a saída
-	var queue = []
+	var fila = []
 	var visited_cells = {}
 	var parent_map = {}
-	queue.append(start_cell)
+	fila.append(start_cell)
 	visited_cells[start_cell] = true
-	while queue:
-		var current = queue.pop_front()
-		print('queue: ' + str(queue))
-		print('Current: ' + str(current))
+	while fila:
+		var current = fila.pop_front()
+		#print('fila: ' + str(fila))
+		#print('Current: ' + str(current))
 		if current == exit_cell:
 			# Construa o caminho a partir dos parentes
 			var path = []
 			var node = exit_cell
 			while node in parent_map:
+				print('Node: ' + str(node))
 				path.append(node)
 				node = parent_map[node]
+				print('Node 2: ' + str(node))
 			# Adicione a célula inicial ao caminho
 			path.append(start_cell) 
 			# Reverter o caminho para começar do início
-			path.reverse() 
+			path.reverse()
 			# Remova as paredes entre o caminho
 			for i in range(path.size() - 1):
-				print('Removendo parede [' + str(path[i]) + ',' + str(path[i+1]) + ']' )
+				#print('Removendo parede [' + str(path[i]) + ',' + str(path[i+1]) + ']' )
 				remove_wall2(path[i], path[i + 1])
 			return
 		var neighbors = get_adjacent_cells(current)
 		for neighbor in neighbors:
 			if neighbor not in visited_cells:
-				queue.append(neighbor)
+				fila.append(neighbor)
 				visited_cells[neighbor] = true
 				parent_map[neighbor] = current
 
@@ -136,16 +145,16 @@ func remove_wall(cell1, cell2):
 	var diff = cell2 - cell1
 	if diff == Vector2(1, 0): # Direita
 		if cell1.x * 2 + 1 < maze.size() and cell1.y * 2 < maze[0].size():
-			maze[cell1.x * 2 + 1][cell1.y * 2] = 1
+			maze[cell1.x * 2 + 1][cell1.y * 2] = 0
 	elif diff == Vector2(-1, 0): # Esquerda
 		if cell2.x * 2 + 1 < maze.size() and cell2.y * 2 < maze[0].size():
-			maze[cell2.x * 2 + 1][cell2.y * 2] = 1
+			maze[cell2.x * 2 + 1][cell2.y * 2] = 0
 	elif diff == Vector2(0, 1): # Baixo
 		if cell1.x * 2 < maze.size() and cell1.y * 2 + 1 < maze[0].size():
-			maze[cell1.x * 2][cell1.y * 2 + 1] = 1
+			maze[cell1.x * 2][cell1.y * 2 + 1] = 0
 	elif diff == Vector2(0, -1): # Cima
 		if cell2.x * 2 < maze.size() and cell2.y * 2 + 1 < maze[0].size():
-			maze[cell2.x * 2][cell2.y * 2 + 1] = 1
+			maze[cell2.x * 2][cell2.y * 2 + 1] = 0
 
 func remove_wall2(cell1, cell2):
 	# Pega a diferença entre as duas celulas adquiridas
@@ -177,7 +186,9 @@ func display_maze():
 			if x == player_start.x and y == player_start.y:
 				tilemap.set_cell(0, Vector2(x,y), 2, Vector2(7,1))
 			# Criando paredes no extremo do mapa
+			if x == 0 or x == map_size.x - 1 or y == 0 or y == map_size.y - 1:
+				tilemap.set_cell(0, Vector2(x,y), 2, Vector2(1,1))
+			# Criando saida
 			if x == exit_point.x-1 and y == exit_point.y-1:
 				tilemap.set_cell(0, Vector2(x,y), 2, Vector2(7,1))
 				saida.position =  Vector2(((16 * (exit_point.x-1))) + 3, ((16 * (exit_point.y-1))) + 3)
-				print(saida.position)
